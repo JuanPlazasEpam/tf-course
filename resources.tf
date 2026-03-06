aa56a2bb01c1ede9fb6c7e6d55edfbeecd7cb7b9
Juan Plazas <juan_plazas@epam.com>
2026-03-06 13:21:44 -0500

    UPDATE | task12

--- resources.tf
+++ resources.tf
@@ -1,8 +1,30 @@
-data "aws_iam_policy" "cmtr_policy" {
-  arn = var.iam_policy_arn
+resource "aws_iam_policy" "cmtr_policy" {
+  name        = "cmtr-gqdgh5re-iam-policy"
+  description = "Custom role with limited permissions"
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": "sts:GetCallerIdentity",
+      "Resource": "*"
+    }
+  ]
 }
+POLICY
+
+  tags = {}
+
+  lifecycle {
+    ignore_changes  = [policy]
+    prevent_destroy = true
+  }
+}
+
 output "cmtr_iam_policy_arn" {
-  description = "The ARN of the existing IAM policy cmtr-gqdgh5re-iam-policy"
-  value       = data.aws_iam_policy.cmtr_policy.arn
+  description = "The ARN of the IAM policy managed in this configuration"
+  value       = aws_iam_policy.cmtr_policy.arn
 }
 
